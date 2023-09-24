c9e93af09bc518074150135d3cc4521b4408165a
skdding3 <skdding3@gmail.com>
2023-09-24 13:37:26 +0900

    event add

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import { styled } from "styled-components";
 
-function App() {
-  const [count, setCount] = useState(0)
+// TYPE
+
+type Props = {
+  title?: string;
+  contents?: string;
+};
+
+// STYLE
+const Container = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+
+  height: 32px;
+  margin: 0 32px 0 32px;
+`;
+
+const Button = styled.div`
+  top: 8px;
+  right: 8px;
+  font-size: 14px;
+  position: absolute;
+`;
+
+const ContentWrapper = styled.div`
+  height: 0;
+  width: 100%;
+  overflow: hidden;
+  transition: height 0.35s ease;
+`;
+
+const Contents = styled.div``;
+
+function App(props: Props) {
+  // Ref
+
+  const parentRef = useRef(null);
+  const childRef = useRef(null);
+
+  // STATE
+  const [isCollaps, setIsCollaps] = useState(false);
+
+  // VIEW
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Header>{props.title}</Header>
+      <ContentWrapper ref={parentRef}>
+        <Contents ref={childRef}>{props.contents}</Contents>
+      </ContentWrapper>
+      <Button>열기</Button>
+    </Container>
+  );
 }
 
-export default App
+export default App;
